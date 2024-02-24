7efba2cf9dc021b4d1edb4f7b3756b0d888de147
Pham <stevenpham1996@outlook.com>
2024-02-25 01:32:29 +0700

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Homepage",
+    "description": "Customized Homepage",
+    "sequence": 10,
+    "version": "1.0",
+    "depends": [
+        "base",
+        "website"
+    ],
+    "data": [],
+    "images": [
+    ],
+    "snippet_lists": {
+    },
+    "application": False,
+    "auto_install": True,
+    "license": "LGPL-3",
+}
